9beca0292b49f24e0c9989c30d12ed48bd9ca152
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-04-25 12:32:52 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.32 ms, Memory: 74.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ Java/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        if (Integer.valueOf(String.valueOf(a)+String.valueOf(b))>Integer.valueOf(String.valueOf(b)+String.valueOf(a))) answer=Integer.valueOf(String.valueOf(a)+String.valueOf(b));
-        else if (Integer.valueOf(String.valueOf(a)+String.valueOf(b))<Integer.valueOf(String.valueOf(b)+String.valueOf(a)));
-        else answer=Integer.valueOf(String.valueOf(a)+String.valueOf(b));
-        return answer;
+		int ab=Integer.valueOf(String.valueOf(a)+String.valueOf(b));
+		int ba=Integer.valueOf(String.valueOf(b)+String.valueOf(a));
+        
+        return (ab>ba)?ab:ba;
     }
 }
