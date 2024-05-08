2996059f5d306fd4925ef6f829228fdda9fa733c
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-08 12:39:29 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 13.87 ms, Memory: 88 MB -BaekjoonHub

--- Java/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ Java/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -2,31 +2,28 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
+        		int[] answer = new int[queries.length];
+		for(int i=0;i<queries.length;i++) {
             int s = queries[i][0];
             int e = queries[i][1];
             int k = queries[i][2];
-            
-            // 쿼리 범위와 k 값을 저장할 임시 배열 생성
-            int[] tmp = new int[e - s + 2];
-            for (int j = s; j <= e; j++) {
-                tmp[j - s] = arr[j];
-            }
-            tmp[e - s + 1] = k; // k 값 저장
-            
-            Arrays.sort(tmp); // 배열 정렬
-            
-            // k 값보다 큰 첫 번째 원소를 찾아 결과로 사용
-            int result = -1;
-            for (int num : tmp) {
-                if (num > k) {
-                    result = num;
-                    break;
-                }
-            }
-            answer[i] = result;
-        }
+			int queries_len=queries[i][1]-queries[i][0]+1;
+			int[] tmp=new int[queries_len+1];
+			for(int j=s;j<=e;j++) {
+				tmp[j-s]=arr[j];
+			}
+			tmp[e-s+1]=k;
+			Arrays.sort(tmp);
+			for(int m=0;m<tmp.length;m++) {
+				if(tmp[m]>queries[i][2]) {
+					answer[i]=tmp[m];
+					break;
+				}
+				else{
+					answer[i]=-1;
+				}
+			}
+		}
         return answer;
     }
 }
