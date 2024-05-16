70cb7b1848d315ba78aad9c15d161114e927a795
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-16 09:35:07 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=0;
+		int max=0,max_index=0;
+		for(int i=0;i<9;i++) {
+			n=in.nextInt();
+			if (n>max) {
+				max=n;
+				max_index=i;
+			}
+		}
+		System.out.println(max);
+		System.out.println(max_index+1);	
+	}
+} 
