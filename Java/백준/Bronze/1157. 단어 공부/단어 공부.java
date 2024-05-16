1e7ca30813f4988145494e64956c70db5305733c
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-16 18:17:06 +0900

    [Bronze I] Title: 단어 공부, Time: 520 ms, Memory: 30660 KB -BaekjoonHub

--- Java/백준/Bronze/1157. 단어 공부/단어 공부.java
+++ Java/백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -2,27 +2,29 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
-		int[] arr=new int[26];
-		int max=0;
-		int count=0;
+		Scanner in =new Scanner(System.in);
 		String input=in.nextLine();
+		in.close();
+		int[] arr= new int[26];
 		for(int i=0;i<input.length();i++) {
-			if(input.charAt(i)<='Z') {
-				arr[input.charAt(i)-'A']++;
+			if (input.charAt(i)<='Z') {
+				arr[input.charAt(i)-65]++;
 			}
 			else {
-				arr[input.charAt(i)-'a']++;
+				arr[input.charAt(i)-97]++;
 			}
 		}
-		char ch='?';
+		int max=-1,max_i=0,count=0;
 		for(int i=0;i<26;i++) {
 			if(arr[i]>max) {
 				max=arr[i];
-				ch=(char)(i+65);
+				max_i=i;
+				count=0;
 			}
-			else if(arr[i]==max) ch='?';
+			else if(arr[i]==max) {
+				count++;
+			}
 		}
-		System.out.print(ch);
+		System.out.print((count>0)?"?":(char)(max_i+65));
 	}
-}
+} 
