9036f0ef5ac44ec3d1b2bcacd1704b43eded381e
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-17 12:15:31 +0900

    [Silver V] Title: 색종이, Time: 216 ms, Memory: 17760 KB -BaekjoonHub

--- Java/백준/Silver/2563. 색종이/색종이.java
+++ Java/백준/Silver/2563. 색종이/색종이.java
@@ -2,24 +2,31 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner in =new Scanner(System.in);
-		int a=in.nextInt();
-		int result=0;
-		int[][] paper =new int[100][100];
-		for(int i=0;i<a;i++) {
+		Scanner in= new Scanner(System.in);
+		
+		boolean[][] paper=new boolean[100][100];
+		for(int i=0;i<100;i++) { //도화지 배열 false로 초기화
+			for(int j=0;j<100;j++) {
+				paper[i][j]=false;
+			}
+		}
+		
+		int n=in.nextInt();
+		for(int i=0;i<n;i++) { //검은색 색종이가 있는 위치를 true으로 만듬
 			int x=in.nextInt();
 			int y=in.nextInt();
 			for(int j=x;j<x+10;j++) {
 				for(int k=y;k<y+10;k++) {
-					paper[j][k]=1;
+					paper[j][k]=true;
 				}
 			}
 		}
-		for(int i=0;i<100;i++) {
+		int area=0;
+		for(int i=0;i<100;i++) { //true인 부분 찾기
 			for(int j=0;j<100;j++) {
-				if(paper[i][j]==1) result++;
+				if (paper[i][j]==true) area++;
 			}
 		}
-		System.out.print(result);
+		System.out.println(area);
 	}
-}
+} 
