b5f54e95fae7fb6c7ecb63703008723ccc9b2f2f
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-17 11:57:17 +0900

    [Bronze I] Title: 세로읽기, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

--- Java/백준/Bronze/10798. 세로읽기/세로읽기.java
+++ Java/백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -2,30 +2,27 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner in =new Scanner(System.in);
-		String[] a=new String[5];
-		
-		String answer="";
-		for(int i=0;i<5;i++) {
-			a[i]=in.nextLine();
-		}
+		Scanner in= new Scanner(System.in);
+		String[] n= new String[5];
 		int max=0;
-		for(int i=0;i<5;i++) {
-			if(a[i].length()>max) max=a[i].length();
-		}
-		char[][] b=new char[5][max];
-		for(int i=0;i<5;i++) {
-			for(int j=0;j<a[i].length();j++) {
-				b[i][j]=a[i].charAt(j);
+		for(int i = 0; i < 5; i++) { 
+			n[i]=in.nextLine();
+			if(n[i].length()>max) {
+				max=n[i].length(); //입력된 단어중 가장 긴 단어의 길이 저장
 			}
 		}
-		for(int i=0;i<max;i++) {
-			for(int j=0;j<a.length;j++) {
-				if (b[j][i] !='\u0000') {
-					answer+=b[j][i];
-				}
+		in.close();
+		
+		char[][] text=new char[5][max];
+		for(int i=0;i<5;i++) { //단어를 2차원 char에 저장
+			for(int j=0;j<n[i].length();j++) {
+				text[i][j]=n[i].charAt(j);
 			}
 		}
-		System.out.println(answer);
+		for(int i=0;i<max;i++) { //출력
+			for(int j=0;j<5;j++) {
+				if (text[j][i]!='\u0000') System.out.print(text[j][i]);
+			}
+		}
 	}
-}
+} 
