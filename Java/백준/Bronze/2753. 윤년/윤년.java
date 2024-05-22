99acf9c77473d4894baf58a93649056e0a76e929
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-22 11:11:02 +0900

    [Bronze V] Title: 윤년, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

--- Java/백준/Bronze/2753. 윤년/윤년.java
+++ Java/백준/Bronze/2753. 윤년/윤년.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int a= in.nextInt();
-		if(a%4==0&& a%100!=0) {
-			System.out.print("1");
-		}
-		else if(a%400==0){
-			System.out.print("1");
+		Scanner in =new Scanner(System.in);
+		int n=in.nextInt();
+		in.close();
+		if((n%4==0&&n%100!=0)||n%400==0) {
+			System.out.println("1");
 		}else {
-			System.out.print("0");
+			System.out.println("0");
 		}
 	}
 }
