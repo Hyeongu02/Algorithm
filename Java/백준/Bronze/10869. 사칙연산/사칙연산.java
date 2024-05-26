01210ad65d18f0af9258488b7304ce21839d66e8
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-26 21:48:55 +0900

    [Bronze V] Title: 사칙연산, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

--- Java/백준/Bronze/10869. 사칙연산/사칙연산.java
+++ Java/백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner in =new Scanner(System.in);
-		int a=in.nextInt();
-		int b=in.nextInt();
-		in.close();
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
 		System.out.println(a+b);
 		System.out.println(a-b);
 		System.out.println(a*b);
 		System.out.println(a/b);
 		System.out.println(a%b);
+		
 	}
 }
