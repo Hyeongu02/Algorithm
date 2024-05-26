cc437877e67d9c7dafa7ce4a262f8472b718d5a3
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-26 22:57:07 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1004 ms, Memory: 288652 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long t=Long.parseLong(br.readLine());
+		for(int i=0;i<t;i++) {
+			String[] str= br.readLine().split(" ");
+			bw.write(Integer.parseInt(str[0])+Integer.parseInt(str[1])+"\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
