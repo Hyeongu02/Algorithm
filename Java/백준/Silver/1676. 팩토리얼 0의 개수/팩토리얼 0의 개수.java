e029c81a206b1ae0c9a4e542a31b9b25f3aecc6f
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-30 17:36:56 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

--- Java/백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ Java/백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,28 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in =new Scanner(System.in);
-		int n=in.nextInt();
-		in.close();
-		int count2=0;
-		int count5=0;
-		for(int i=1;i<=n;i++) {
-			if(i%2==0 ||i%5==0) {
-				int tmp=i;
-				while(true) {
-					if(tmp%2==0) {
-						count2++;
-						tmp/=2;
-					}
-					if(tmp%5==0) {
-						count5++;
-						tmp/=5;
-					}
-					if(tmp%2!=0 &&tmp%5!=0) break;
-				}
-			}
-		}
-		System.out.println((count2<=count5)?count2:count5);
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        
+        int count5 = 0;
+        for (int i = 5; i <= n; i *= 5) {
+            count5 += n / i;
+        }
+        
+        System.out.println(count5);
+    }
 }
