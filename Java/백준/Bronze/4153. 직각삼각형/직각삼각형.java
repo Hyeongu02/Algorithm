a1bafc05469065a94549b05082cd801a1a5c64c2
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-31 12:34:00 +0900

    [Bronze III] Title: 직각삼각형, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

---
+++ Java/백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			String[] n= br.readLine().split(" ");
+			int[] arr=new int[n.length];
+			for(int i=0;i<n.length;i++) {
+				arr[i]=Integer.parseInt(n[i]);
+			}
+			Arrays.sort(arr);
+			if(arr[2]==0) break;
+			if(arr[0]*arr[0]+arr[1]*arr[1]==arr[2]*arr[2]) System.out.println("right");
+			else System.out.println("wrong");
+		}
+	}
+}
