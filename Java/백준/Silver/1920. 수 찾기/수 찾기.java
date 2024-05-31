be84501ce1588ed578801f44fb5ab080732b5936
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-05-31 18:23:01 +0900

    [Silver IV] Title: 수 찾기, Time: 656 ms, Memory: 42064 KB -BaekjoonHub

--- Java/백준/Silver/1920. 수 찾기/수 찾기.java
+++ Java/백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,38 +1,78 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.StringTokenizer;
  
 public class Main {
+ 
+	public static int[] arr;
+	
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] n=new int[Integer.parseInt(br.readLine())];
-		StringTokenizer st= new StringTokenizer(br.readLine()," ");
-		for(int i=0;i<n.length;i++) {
-			n[i]=Integer.parseInt(st.nextToken());
+		
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(n);
-		int[] m=new int[Integer.parseInt(br.readLine())];
-		st= new StringTokenizer(br.readLine()," ");
-		for(int i=0;i<m.length;i++) {
-			m[i]=Integer.parseInt(st.nextToken());
-		}
-		for(int i:m) {
-			int max=n.length-1;
-			int min=0;
-			boolean find =false;
-			while(max>=min) {
-				int mid=(max+min)/2;
-				if(n[mid]>i) max=mid-1;
-				else if(n[mid]<i) min=mid+1;
-				else {
-					System.out.println("1");
-					find=true;
-					break;
-				}
+		
+		
+		// 배열은 반드시 정렬되어있어야한다.
+		Arrays.sort(arr);
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < M; i++) {
+			
+			// 찾고자 하는 값이 있을 경우 1, 없을 경우 0을 출력해야한다.
+			if(binarySearch(Integer.parseInt(st.nextToken())) >= 0) {
+				sb.append(1).append('\n');
 			}
-			if (find==false) System.out.println("0");
+			else {
+				sb.append(0).append('\n');
+			}
 		}
+		System.out.println(sb);
+	}
+	
+	
+	/**
+	 * @param key 찾으려는 값
+	 * @return key와 일치하는 배열의 인덱스
+	 */
+	public static int binarySearch(int key) {
+ 
+		int lo = 0;					// 탐색 범위의 왼쪽 끝 인덱스
+		int hi = arr.length - 1;	// 탐색 범위의 오른쪽 끝 인덱스
+ 
+		// lo가 hi보다 커지기 전까지 반복한다.
+		while(lo <= hi) {
+ 
+			int mid = (lo + hi) / 2;	// 중간위치를 구한다.
+ 
+			// key값이 중간 위치의 값보다 작을 경우
+			if(key < arr[mid]) {
+				hi = mid - 1;
+			}
+			// key값이 중간 위치의 값보다 클 경우
+			else if(key > arr[mid]) {
+				lo = mid + 1;
+			}
+			// key값과 중간 위치의 값이 같을 경우
+			else {
+				return mid;
+			}
+		}
+ 
+		// 찾고자 하는 값이 존재하지 않을 경우
+		return -1;
+ 
 	}
 }
