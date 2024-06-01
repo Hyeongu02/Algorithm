346eb1d65a6b241b4a9589abb9dda95433c504cb
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-06-01 18:38:11 +0900

    [Bronze I] Title: 소인수분해, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- Java/백준/Bronze/11653. 소인수분해/소인수분해.java
+++ Java/백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,23 +1,18 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
- 
-		for (int i = 2; i <= Math.sqrt(N); i++) {
-			while (N % i == 0) {
+		StringBuffer sb= new StringBuffer();
+		int N=Integer.parseInt(br.readLine()); 
+		for(int i=2;i*i<=N;i++) {
+			while(N%i==0) {
 				sb.append(i).append('\n');
-				N /= i;
+				N/=i;
 			}
 		}
-		if (N != 1) {
-			sb.append(N);
-		}
+		if(N!=1) sb.append(N);
 		System.out.println(sb);
 	}
 }
