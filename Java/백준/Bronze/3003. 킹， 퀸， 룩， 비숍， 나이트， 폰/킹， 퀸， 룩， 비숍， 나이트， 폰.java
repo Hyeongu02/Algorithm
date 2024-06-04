271c116faf2856653812abb81be987b6d8ac0891
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-06-04 14:30:45 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

--- Java/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ Java/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,14 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in =new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 		int[] arr= {1,1,2,2,2,8};
-		int n=0;
+		StringTokenizer st=new StringTokenizer(br.readLine());
 		for(int i=0;i<6;i++) {
-			n=in.nextInt();
-			if (n!=arr[i]) System.out.print(arr[i]-n+" ");
-			else System.out.print("0 ");
+			int n=Integer.parseInt(st.nextToken());
+			if(n!=arr[i]) bw.write(Integer.toString(arr[i]-n)+" ");
+			else bw.write("0 ");
 		}
+		bw.flush();
+		bw.close();
 	}
-} 
+}
