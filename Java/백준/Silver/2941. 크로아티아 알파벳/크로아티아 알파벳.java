3446207c6de25168189e7ae5701da87b2e4f05bf
Hyeongu <80089798+Hyeongu-02@users.noreply.github.com>
2024-06-09 19:55:18 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- Java/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ Java/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,54 +1,14 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
-	public static void main(String[] args) {
-		Scanner in =new Scanner(System.in);
-		String input=in.nextLine();
-		in.close();
-		char[] arr=new char[input.length()+1];
-		int count=0;
-		for(int i=0;i<input.length();i++) {
-			arr[i]=input.charAt(i);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String[] target= {"c=","c-","dz=","d-","lj","nj","s=","z="};
+		String str=br.readLine();
+		for(int i=0;i<target.length;i++) {
+			str=str.replaceAll(target[i], "0");
 		}
-		for(int i=0;i<input.length();i++) {
-			if(arr[i]=='c') {
-				if(arr[i+1]=='=') {
-					i++;
-				}
-				else if(arr[i+1]=='-') {
-					i++;
-				}
-			}
-			else if(arr[i]=='d') {
-				if(arr[i+1]=='z'&&arr[i+2]=='=') {
-					i+=2;
-				}	
-				else if(arr[i+1]=='-') {
-					i++;
-				}
-			}
-			else if(arr[i]=='l') {
-				if(arr[i+1]=='j') {
-					i++;
-				}
-			}
-			else if(arr[i]=='n') {
-				if(arr[i+1]=='j') {
-					i++;
-				}
-			}
-			else if(arr[i]=='s') {
-				if(arr[i+1]=='=') {
-					i++;
-				}
-			}
-			else if(arr[i]=='z') {
-				if(arr[i+1]=='=') {
-					i++;
-				}
-			}
-			count++;
-		}
-		System.out.println(count);
+		System.out.println(str.length());
 	}
-} 
+}
