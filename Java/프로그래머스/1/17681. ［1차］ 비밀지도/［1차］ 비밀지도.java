ce5ba28b3a7964d4b05300582b2e3c8f449fc573
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-06-12 19:36:58 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.33 ms, Memory: 67.8 MB -BaekjoonHub

--- Java/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ Java/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -4,14 +4,13 @@ class Solution {
 		for(int i=0;i<n;i++) {
 			StringBuffer sb= new StringBuffer();
 			String tmp=Integer.toBinaryString(arr1[i]|arr2[i]);
-            for(int j=0;j<n-tmp.length();j++) {
+            tmp=tmp.replaceAll("1", "#");
+			tmp=tmp.replaceAll("0", " ");
+			for(int j=0;j<n-tmp.length();j++) {
 				sb.append(' ');
 			}
-			for(int j=0;j<tmp.length();j++) {
-				if(tmp.charAt(j)=='1') sb.append('#');
-				else sb.append(' ');
-			}
-			answer[i]=sb.toString();
+			tmp=sb.toString().concat(tmp);
+			answer[i]=tmp;
 		}
         return answer;
     }
