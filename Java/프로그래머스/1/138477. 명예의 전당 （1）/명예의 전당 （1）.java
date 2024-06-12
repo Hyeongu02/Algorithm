0fe9645b3e5de4611c5cca9e7ff231fa694c6fcf
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-06-12 18:48:55 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.02 ms, Memory: 77.3 MB -BaekjoonHub

--- Java/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ Java/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,18 +1,6 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-
 class Solution {
     public int[] solution(int k, int[] score) {
-        List<Integer> list=new ArrayList<Integer>();
-		List<Integer> tmp=new ArrayList<Integer>();
-		for(int i:score) {
-			list.add(i);
-			list.sort(Comparator.reverseOrder());
-			tmp.add(list.get((list.size()<k)?list.size()-1:k-1));
-		}
-        int[] answer = tmp.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = {};
         return answer;
     }
 }
