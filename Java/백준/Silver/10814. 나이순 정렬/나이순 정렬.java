4c918f09ac0ec0edcf5fa53a5c89e412fdb87e7e
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-06-14 19:10:58 +0900

    [Silver V] Title: 나이순 정렬, Time: 440 ms, Memory: 39224 KB -BaekjoonHub

--- Java/백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ Java/백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,22 +1,28 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException{
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
 		int n=Integer.parseInt(br.readLine());
-		String[][] member=new String[n][2];
-		for(int i=0;i<n;i++) {
-			member[i]=br.readLine().split(" ");
+		StringBuilder[] sb= new StringBuilder[201];
+		for (int i = 0; i < sb.length; i++) {
+			sb[i] = new StringBuilder();
 		}
-	
-		Arrays.sort(member,(arr1, arr2) ->Integer.compare(Integer.parseInt(arr1[0]), Integer.parseInt(arr2[0])));
-		StringBuilder sb= new StringBuilder();
 		for(int i=0;i<n;i++) {
-			sb.append(member[i][0]).append(' ').append(member[i][1]).append('\n');
+			StringTokenizer st=new StringTokenizer(br.readLine()," ");
+			int age=Integer.parseInt(st.nextToken());
+			sb[age].append(age).append(' ').append(st.nextToken()).append('\n');
 		}
-		System.out.println(sb);
+		for(int i=0;i<sb.length;i++) {
+			bw.append(sb[i]);
+		}
+		bw.flush();
+		bw.close();
 	}
 }
