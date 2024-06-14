f6ec33848f46013cf82f0af5427d8776e5877470
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-06-14 13:57:57 +0900

    [Silver V] Title: 나이순 정렬, Time: 1768 ms, Memory: 66684 KB -BaekjoonHub

---
+++ Java/백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		String[][] member=new String[n][2];
+		for(int i=0;i<n;i++) {
+			member[i]=br.readLine().split(" ");
+		}
+	
+		Arrays.sort(member,(arr1, arr2) ->Integer.compare(Integer.parseInt(arr1[0]), Integer.parseInt(arr2[0])));
+		for(int i=0;i<n;i++) {
+			System.out.println(member[i][0]+" "+member[i][1]);
+		}
+	}
+}
