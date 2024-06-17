07c13b0ea1552a21caab0ca01f4a65b44c323917
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-06-17 18:53:11 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- Java/백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ Java/백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayList;
  
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -9,7 +9,7 @@ public class Main {
 		String[] input=br.readLine().split(" ");
 		int n=Integer.parseInt(input[0]);
 		int k=Integer.parseInt(input[1]);
-		LinkedList<Integer> list =new LinkedList<Integer>();
+		ArrayList<Integer> list =new ArrayList<Integer>();
 		for(int i=0;i<n;i++) {
 			list.add(i+1);
 		}
