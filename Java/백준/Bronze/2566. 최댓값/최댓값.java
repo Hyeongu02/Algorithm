f40c585dbe2f7aff9e1984055847206c82db17a2
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-06-19 12:19:32 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

--- Java/백준/Bronze/2566. 최댓값/최댓값.java
+++ Java/백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,20 +1,28 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 public class Main {
-	public static void main(String[] args) {
-		Scanner in =new Scanner(System.in);
-		int a=0,max=0,x=0,y=0;
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb=new StringBuilder();
+		int max=-1;
+		int max_x=0;
+		int max_y=0;
 		for(int i=0;i<9;i++) {
+			st=new StringTokenizer(br.readLine()," ");
 			for(int j=0;j<9;j++) {
-				a=in.nextInt();
-				if(a>max) {
-					max=a;
-					x=i;
-					y=j;
+				int n=Integer.parseInt(st.nextToken());
+				if(n>max) {
+					max=n;
+					max_x=i+1;
+					max_y=j+1;
 				}
 			}
 		}
-		System.out.println(max);
-		System.out.println((x+1)+" "+(y+1));
+		sb.append(max).append('\n');
+		sb.append(max_x).append(" ").append(max_y);
+		System.out.println(sb);
 	}
 }
