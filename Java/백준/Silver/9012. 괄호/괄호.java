c529cf1cf23462ede7bd0946ce445f50a270d9e1
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-06-19 18:53:56 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- Java/백준/Silver/9012. 괄호/괄호.java
+++ Java/백준/Silver/9012. 괄호/괄호.java
@@ -12,19 +12,18 @@ public class Main {
 		for(int i=0;i<t;i++) {
 			Stack<Character>stack=new Stack<Character>();
 			str=br.readLine();
-			if(str.charAt(0)==')') { //첫번째가 )이면 바로 NO
+			if(str.charAt(0)==')') {
 				sb.append("NO").append('\n');
 				continue;
 			}
-			stack.push(str.charAt(0));
-			for(int j=1;j<str.length();j++) {
-				if(!stack.empty()&&stack.peek()!=str.charAt(j)) { //스택이 비어있지 않고 이전것과 다르면
-					stack.pop();
-				}else if(stack.empty()&&str.charAt(j)==')') { //스택이 비워져있는 상태에서 )가 들어올때
+			for(int j=0;j<str.length();j++) {
+				if(str.charAt(j)=='(') { 
+					stack.push(str.charAt(j));
+				}else if(stack.empty()){
 					stack.push('1');
 					break;
-				}else {//비어저있거나 이전것과 같을때
-					stack.push(str.charAt(j));
+				}else {
+					stack.pop();
 				}
 			}
 			if(stack.size()==0) sb.append("YES").append('\n');
