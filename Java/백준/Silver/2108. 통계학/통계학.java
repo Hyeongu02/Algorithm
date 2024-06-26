a45283f526210fc9c6ca8b432b90f412b95f8e61
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-06-26 19:27:29 +0900

    [Silver III] Title: 통계학, Time: 344 ms, Memory: 41208 KB -BaekjoonHub

--- Java/백준/Silver/2108. 통계학/통계학.java
+++ Java/백준/Silver/2108. 통계학/통계학.java
@@ -3,57 +3,53 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.LinkedList;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[8001];
-        int max = -4000;
-        int min = 4000;
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            int input = Integer.parseInt(br.readLine());
-            nums[input + 4000]++;
-            if (input > max) max = input;
-            if (input < min) min = input;
-            sum += input;
-        }
-
-        // 최빈값
-        int maxFrequency = 0;
-        LinkedList<Integer> list = new LinkedList<>();
-        for (int i = min + 4000; i <= max + 4000; i++) {
-            if (nums[i] > maxFrequency) {
-                list.clear();
-                maxFrequency = nums[i];
-                list.add(i - 4000);
-            } else if (nums[i] == maxFrequency) {
-                list.add(i - 4000);
-            }
-        }
-        Collections.sort(list); // 정렬한 뒤에 list size에 따라 출력
-
-        // 중앙값
-        int count = 0;
-        int mid = 0;
-        Loop: for (int i = min + 4000; i <= max + 4000; i++) {
-            while (nums[i] != 0) {
-                nums[i]--;
-                count++;
-                if (count == n / 2 + 1) {
-                    mid = i - 4000;
-                    break Loop;
-                }
-            }
-        }
-
-        // 출력
-        System.out.println(Math.round(sum / (double) n));
-        System.out.println(mid);
-        if (list.size() > 1) System.out.println(list.get(1));
-        else System.out.println(list.get(0));
-        System.out.println(max - min);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in)); 
+		int n= Integer.parseInt(br.readLine());
+		int[] nums=new int[8001];
+		int max=-4000;
+		int min=4000;
+		int input=0;
+		int sum=0;
+		for(int i=0;i<n;i++) {
+			input=Integer.parseInt(br.readLine());
+			nums[input+4000]++;
+			if(input>max)max=input;//최대
+			if(input<min)min=input;//최소
+			sum+=input;//총합
+		}
+		//최빈값
+		int arrMax=0;
+		LinkedList<Integer>list=new LinkedList<Integer>();
+		for(int i=min+4000;i<=max+4000;i++) {
+			if(nums[i]>arrMax) {
+				list=new LinkedList<Integer>();
+				arrMax=nums[i];
+				list.add(i-4000);
+			}else if(nums[i]==arrMax) {
+				list.add(i-4000);
+			}
+		}
+		Collections.sort(list); //정렬한뒤에 list size에 따라 출력
+		//중앙값
+		int count=0;
+		int mid=0;
+		Loop: for(int i=min+4000;i<=max+4000;i++) {
+			while(nums[i]!=0) {
+				nums[i]--;
+				count++;
+				if(count==n/2+1) {
+					mid=i-4000;
+					break Loop;
+				}
+			}
+		}
+		//출력
+		System.out.println(Math.round(sum/(double)n));
+		System.out.println(mid);
+		if(list.size()==1) System.out.println(list.peek());
+		else System.out.println(list.get(1));
+		System.out.println(max-min);
+	}
 }
