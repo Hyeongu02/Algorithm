185b9a33c686f6c945eb5f1f6ef06a61a9f6641c
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2024-07-04 17:57:58 +0900

    [Bronze V] Title: Hook, Time: 108 ms, Memory: 14008 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb=new StringBuilder();
+		sb.append("#  # #### #### #  #").append('\n');
+		sb.append("#### #  # #  # # #").append('\n');
+		sb.append("#### #  # #  # # #").append('\n');
+		sb.append("#  # #### #### #  #").append('\n');
+		System.out.println(sb);
+	}
+}
---
+++ Java/백준/Bronze/10189. Hook/README.md
@@ -0,0 +1,28 @@
+# [Bronze V] Hook - 10189 
+
+[문제 링크](https://www.acmicpc.net/problem/10189) 
+
+### 성능 요약
+
+메모리: 14008 KB, 시간: 108 ms
+
+### 분류
+
+구현
+
+### 제출 일자
+
+2024년 7월 4일 17:57:51
+
+### 문제 설명
+
+<p>Print out the word Hook as shown below.</p>
+
+### 입력 
+
+ Empty
+
+### 출력 
+
+ <p>Print out the word Hook as shown below.</p>
+
