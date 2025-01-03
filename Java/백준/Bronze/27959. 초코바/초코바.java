b952e0c53514db621e9dbde9217a0c963025ee0f
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2025-01-03 22:14:25 +0900

    [Bronze V] Title: 초코바, Time: 176 ms, Memory: 17728 KB -BaekjoonHub

---
+++ Java/백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+		int m= sc.nextInt();
+		if(n*100/m>=1) {
+			System.out.print("Yes");
+		}else {
+			System.out.print("No");
+		}
+	}
+}
+
