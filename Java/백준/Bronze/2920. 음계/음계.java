400d887c6b18c041c1f113ee20d655be102d52d4
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2025-04-23 17:26:27 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

--- Java/백준/Bronze/2920. 음계/음계.java
+++ Java/백준/Bronze/2920. 음계/음계.java
@@ -1,20 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in =new Scanner(System.in);
-		int[] n=new int[8];
-		for(int i=0;i<8;i++) {
-			n[i]=in.nextInt();
-		}
-		in.close();
-		for(int i=1;i<8;i++) {
-			if (n[i-1]-n[i]!=-1&&n[i-1]-n[i]!=1) {
-				System.out.println("mixed");
-				return;
-			}
-		}
-		if (n[0]==1) System.out.println("ascending");
-		else if (n[0]==8) System.out.println("descending");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] ascending = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] descending = {8, 7, 6, 5, 4, 3, 2, 1};
+
+        String[] input = br.readLine().split(" ");
+        boolean isAsc = true;
+        boolean isDesc = true;
+
+        for (int i = 0; i < input.length; i++) {
+            int num = Integer.parseInt(input[i]);
+            if (num != ascending[i]) isAsc = false;
+            if (num != descending[i]) isDesc = false;
+        }
+
+        if (isAsc) {
+            System.out.println("ascending");
+        } else if (isDesc) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+    }
 }
