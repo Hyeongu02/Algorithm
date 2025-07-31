4d7169bd72430e3cd2d63a5ec3fb20405fe2e459
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2025-07-31 11:16:54 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 116 ms, Memory: 14260 KB -BaekjoonHub

--- Java/백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ Java/백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -7,32 +7,23 @@ public class Main{
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
         int k = Integer.parseInt(input[1]);
-        List<Integer> list = new LinkedList<>();
-        List<Integer> answerList = new LinkedList<>();
-        for(int i=1;i<=n;i++){
+        
+        List<Integer> list = new ArrayList<>(n);
+        for(int i=1; i<n+1; i++){
             list.add(i);
         }
         StringBuilder sb = new StringBuilder();
         sb.append("<");
-        ListIterator<Integer> cusor = list.listIterator();
-        while(list.size()>0){
-            for(int i=0;i<k;i++){
-                if(!cusor.hasNext())cusor = list.listIterator();
-                cusor.next();
-            }
-            if (!cusor.hasPrevious()) {
-                cusor = list.listIterator(list.size());
-            }
-            int removed = cusor.previous(); 
-            cusor.next(); 
-            cusor.remove(); 
-
-            if (list.isEmpty()) {
-                sb.append(removed).append(">");
-            } else {
-                sb.append(removed).append(", ");
+        int index = 0;
+        while(!list.isEmpty()){
+            index = (index+k-1)%list.size();
+            int del = list.remove(index);
+            sb.append(del);
+            if(!list.isEmpty()){
+                sb.append(", ");
             }
         }
-        System.out.print(sb.toString());
+        sb.append(">");
+        System.out.println(sb);
     }
 }
