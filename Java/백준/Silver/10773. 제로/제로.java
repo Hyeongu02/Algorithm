0472679ec7f78d0e6a02d78c09c0fd9d30abb8d2
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2025-07-31 11:47:48 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 23928 KB -BaekjoonHub

--- Java/백준/Silver/10773. 제로/제로.java
+++ Java/백준/Silver/10773. 제로/제로.java
@@ -1,25 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in)); 
-		Stack<Integer> stack = new Stack<>();
-		int k=Integer.parseInt(br.readLine());
-		for(int i=0;i<k;i++) {
-			int input=Integer.parseInt(br.readLine());
-			if(input==0) {
-				stack.pop();
-				continue;
-			}
-			stack.add(input);
-		}
-		int sum=0;
-		while (!stack.empty()) {
-			sum+=stack.pop();
-		}
-		System.out.println(sum);
-	}
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        Deque<Integer> que = new ArrayDeque<>();
+        for(int i=0; i<k; i++){
+            int x = Integer.parseInt(br.readLine());
+            if(x!=0){
+                que.push(x);
+            }else{
+                que.pop();
+            }
+        }
+        int sum=0;
+        while(!que.isEmpty()){
+            sum += que.pop();
+        }
+        System.out.print(sum);
+    }
 }
