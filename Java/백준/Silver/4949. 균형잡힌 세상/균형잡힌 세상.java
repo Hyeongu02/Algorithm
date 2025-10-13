acf5fafe8e3e6a5de26aaa7d678bfe3954b98092
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2025-10-13 11:01:11 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 192 ms, Memory: 19824 KB -BaekjoonHub

--- Java/백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ Java/백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,41 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in)); 
-		StringBuilder sb=new StringBuilder();
-		Loop:while(true) {
-			Stack<Character> stack = new Stack<>();
-			String str=br.readLine();
-			Boolean flag=false;
-			if(str.equals(".")) break; //.있으면 멈춤
-			for(int i=0;i<str.length();i++) { //문자열 길이만큼 반복
-				if(str.charAt(i)=='['||str.charAt(i)=='(') {
-					stack.push(str.charAt(i));
-				}
-				if(str.charAt(i)==']') {
-					if(stack.isEmpty()||stack.peek()=='(') {
-						sb.append("no").append('\n');
-						continue Loop;
-					}else {
-						stack.pop();
-					}
-				}
-				if(str.charAt(i)==')') {
-					if(stack.isEmpty()||stack.peek()=='[') {
-						sb.append("no").append('\n');
-						continue Loop;
-					}else {
-						stack.pop();
-					}
-				}
-			}
-			if(stack.isEmpty()) sb.append("yes").append('\n');
-			else sb.append("no").append('\n');
-		}
-		System.out.println(sb);
-	}
+public class Main{
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Deque<Character> stack = new ArrayDeque<>();
+        while (true) {
+            char[] input = br.readLine().toCharArray();
+            stack.clear();
+            if(input[0]=='.'){
+                bw.flush();
+                break;
+            }else{
+                for(int i=0; i<input.length; i++){
+                    if(input[i]=='('||input[i]==')'||input[i]=='['||input[i]==']'){
+                        if(!stack.isEmpty()){
+                            if(input[i]==')'&&stack.peekLast()==input[i]-1){
+                                stack.pollLast();
+                            }else if(input[i]==']'&&stack.peekLast()==input[i]-2){
+                                stack.pollLast();
+                            }else{
+                                stack.addLast(input[i]);
+                            }
+                        }else{
+                            stack.addLast(input[i]);
+                        }
+                    }
+                }
+                if(stack.isEmpty()){
+                    bw.append("yes\n");
+                }else{
+                    bw.append("no\n");
+                }
+            }
+        }
+    }
 }
