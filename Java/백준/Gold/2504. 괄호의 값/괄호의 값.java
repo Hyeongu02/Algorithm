80b21fc151b0b5e5a652fa213eacbbd967edd992
Hyeongu <80089798+Hyeongu02@users.noreply.github.com>
2025-10-15 16:29:24 +0900

    [Gold V] Title: 괄호의 값, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

--- Java/백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ Java/백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,100 +1,46 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 public class Main{
-    public static void main(String[]args) throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] input = br.readLine().toCharArray();
-        boolean flag1 = false;
-        boolean flag2 = false;
-        Deque<Long> stack = new ArrayDeque<>();
-        long answer = 0;
-        long tmp = 0;
-        
-        for(char x : input){
-            if(x ==')'){
-                if(flag1){
-                    stack.pop();
-                    stack.push(2L+ 100);
-                }else{
-                    if(stack.isEmpty()){
-                        System.out.println(0);
-                        return;
-                    }
-                    while(!stack.isEmpty()) {
-                        if(stack.peek()=='[') {
-                            System.out.println(0);
-                            return;
-                        }
-                        if(stack.peek()=='('){
-                            stack.pop();
-                            stack.push(tmp*2+100L);
-                            break;
-                        }else{
-                            tmp += stack.pop()-100;
-                        }
-                    }
-                }
-                if(stack.isEmpty()&&tmp>0){
-                    System.out.println(0);
+        Deque<Character> stack = new ArrayDeque<>();
+        String input = br.readLine();
+        int temp = 1;
+        int answer = 0;
+        for(int i=0; i<input.length(); i++){
+            char c = input.charAt(i);
+            if(c=='('){
+                stack.push(c);
+                temp*=2;
+            }else if(c=='['){
+                stack.push(c);
+                temp*=3;
+            }else if(c==')'){
+                if(stack.isEmpty()||stack.peek()!='('){
+                    System.out.print(0);
                     return;
-                }else{
-                    tmp=0;
+                }else if(input.charAt(i-1)=='('){
+                    answer+=temp;
                 }
-                flag1=false;
-                flag2=false;
-            }else if(x ==']'){
-                if(flag2){
-                    stack.pop();
-                    stack.push(3L+ 100);
-                }else{
-                    if(stack.isEmpty()){
-                        System.out.println(0);
-                        return;
-                    }
-                    while(!stack.isEmpty()) {
-                        if(stack.peek()=='(') {
-                            System.out.println(0);
-                            return;
-                        }
-                        if(stack.peek()=='['){
-                            stack.pop();
-                            stack.push(tmp*3+100L);
-                            break;
-                        }else{
-                            tmp += stack.pop()-100;
-                        }
-                    }
-                }
-                if(stack.isEmpty()&&tmp>0){
-                    System.out.println(0);
+                temp/=2;
+                stack.pop();
+            }else if(c==']'){
+                if(stack.isEmpty()||stack.peek()!='['){
+                    System.out.print(0);
                     return;
-                }else{
-                    tmp=0;
+                }else if(input.charAt(i-1)=='['){
+                    answer+=temp;
                 }
-                flag1=false;
-                flag2=false;
-            }else{
-                stack.push((long)x);
-                if(x=='('){
-                    flag1=true;
-                    flag2=false;
-                }else{
-                    flag2=true;
-                    flag1=false;
-                }
+                temp/=3;
+                stack.pop();
             }
         }
-
-        while (!stack.isEmpty()) {
-            if(stack.peek()<100){
-                System.out.println(0);
-                return;
-            }else{
-                answer+=stack.pop()-100;
-            }
+        if(!stack.isEmpty()){
+            System.out.print(0);
+            return;
+        }else{
+            System.out.print(answer);
         }
-        System.out.println(answer);
     }
 }
